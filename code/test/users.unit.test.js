34deb3751a62c9b06c3e61bd3f1df19b9d663d94
Giovanni Bordero <s313010@studenti.polito.it>
2023-05-08 16:20:29 +0200

    Update getUsers on unit test based on the Slack correction

--- code/test/users.unit.test.js
+++ code/test/users.unit.test.js
@@ -23,22 +23,30 @@ beforeEach(() => {
 describe("getUsers", () => {
   test("should return empty list if there are no users", async () => {
     //any time the `User.find()` method is called jest will replace its actual implementation with the one defined below
-    jest.spyOn(User, "find").mockImplementation(() => [])
-    const response = await request(app)
-      .get("/api/users")
-
-    expect(response.status).toBe(200)
-    expect(response.body).toEqual([])
+    const mockReq = {}
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    }
+    jest.spyOn(User, "find").mockResolvedValue([])
+    await getUsers(mockReq, mockRes)
+    expect(User.find).toHaveBeenCalled()
+    expect(mockRes.status).toHaveBeenCalledWith(200)
+    expect(mockRes.json).toHaveBeenCalledWith([])
   })
 
   test("should retrieve list of all users", async () => {
+    const mockReq = {}
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    }
     const retrievedUsers = [{ username: 'test1', email: 'test1@example.com', password: 'hashedPassword1' }, { username: 'test2', email: 'test2@example.com', password: 'hashedPassword2' }]
-    jest.spyOn(User, "find").mockImplementation(() => retrievedUsers)
-    const response = await request(app)
-      .get("/api/users")
-
-    expect(response.status).toBe(200)
-    expect(response.body).toEqual(retrievedUsers)
+    jest.spyOn(User, "find").mockResolvedValue(retrievedUsers)
+    await getUsers(mockReq, mockRes)
+    expect(User.find).toHaveBeenCalled()
+    expect(mockRes.status).toHaveBeenCalledWith(200)
+    expect(mockRes.json).toHaveBeenCalledWith(retrievedUsers)
   })
 })
 
