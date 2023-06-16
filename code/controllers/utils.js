7851b565d1591c4bca5b34f3fb53a3d2334b225d
ANDREA GALELLA s310166 <s310166@studenti.polito.it>
2023-06-16 10:58:10 +0200

    handleDateFilterParams test fix

--- code/controllers/utils.js
+++ code/controllers/utils.js
@@ -11,7 +11,7 @@ import jwt from 'jsonwebtoken'
 export const handleDateFilterParams = (req) => {
     const query = req.query;
 
-    const date_regExp = new RegExp("[0-9]{4}\-[0-9]{2}\-[0-9]{2}");
+    const date_regExp = new RegExp("[0-9]{4}\-(0?[1-9]|1[0-2])\-(0?[1-9]|1[0-9]|2[0-9]|3[0-1])");
     const d = {};
 
     if(query["from"]) {
